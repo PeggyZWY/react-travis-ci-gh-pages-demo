11bc03303050616f9e1dd766ce5434ed97a785a7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-08 03:38:53 +0000

    Deploy PeggyZWY/react-travis-ci-gh-pages-demo to github.com/PeggyZWY/react-travis-ci-gh-pages-demo.git:gh-pages

---
+++ precache-manifest.aa0bdcf1233d005ffa760f30910349a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eba9a2b64bd0ab84d5adc41a81189bf3",
+    "url": "/react-travis-ci-gh-pages-demo/index.html"
+  },
+  {
+    "revision": "a767b5beee0c350a671a",
+    "url": "/react-travis-ci-gh-pages-demo/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4791001ef3e19f064052",
+    "url": "/react-travis-ci-gh-pages-demo/static/js/2.b1eec8b5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-travis-ci-gh-pages-demo/static/js/2.b1eec8b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a767b5beee0c350a671a",
+    "url": "/react-travis-ci-gh-pages-demo/static/js/main.1c37cc5b.chunk.js"
+  },
+  {
+    "revision": "a2d5b980ea7d50e946c0",
+    "url": "/react-travis-ci-gh-pages-demo/static/js/runtime-main.90342efb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-travis-ci-gh-pages-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
